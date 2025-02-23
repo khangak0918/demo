9dadaee408871d010630d099e9a22b807dd6bc1d
Demo <khangak0918@gmail.com>
2025-02-23 14:54:54 +0700

    ứdfg

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
       Đăng nhập
       </button>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Đăng kí 
+      </button>
     </div>
   );
 }
